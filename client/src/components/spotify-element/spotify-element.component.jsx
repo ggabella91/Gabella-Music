abac407318c005fc4a166c0d23817a24fee34291
Giuliano Gabella <ggabella91@gmail.com>
2020-09-10 19:11:11 -0700

    Added links to the artist pages and track pages (which plays the tracks) in the Spotify Web Player for each rendered top artists and top tracks.

--- client/src/components/spotify-element/spotify-element.component.jsx
+++ client/src/components/spotify-element/spotify-element.component.jsx
@@ -4,15 +4,23 @@ import './spotify-element.styles.scss';
 
 const SpotifyElement = ({ type, item }) => {
   if (type === 'artist') {
-    const { name, images } = item;
+    const { name, images, external_urls } = item;
     return (
       <div className='spotify-element'>
         <img className='artist-image' src={images[0].url} alt={name} />
-        <span>{name}</span>
+        <span>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href={external_urls.spotify}
+          >
+            {name}
+          </a>
+        </span>
       </div>
     );
   } else if (type === 'track') {
-    const { album, artists, name } = item;
+    const { album, artists, name, external_urls } = item;
     const artistString =
       artists.length > 1
         ? artists.map((artist) => artist.name).join(', ')
@@ -22,11 +30,17 @@ const SpotifyElement = ({ type, item }) => {
       <div className='spotify-element'>
         <img className='album-image' src={album.images[0].url} alt={name} />
         <div className='details-container'>
-          <ul>
-            <li className='main-text'>{name}</li>
-            <li className='small-text'>{album.name}</li>
-            <li className='small-text'>{artistString}</li>
-          </ul>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href={external_urls.spotify}
+          >
+            <ul>
+              <li className='main-text'>{name}</li>
+              <li className='small-text'>{album.name}</li>
+              <li className='small-text'>{artistString}</li>
+            </ul>
+          </a>
         </div>
       </div>
     );
