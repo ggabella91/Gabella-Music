548dfef169d5283ccdf66d59ad008b3026274ea9
Giuliano Gabella <ggabella91@gmail.com>
2023-02-09 19:15:21 -0600

    Api: Add error handling in send function of email class

--- api/utils/email.js
+++ api/utils/email.js
@@ -51,7 +51,11 @@ module.exports = class Email {
     };
 
     // 3) Create a transport and send email
-    await this.newTransport().sendMail(mailOptions);
+    try {
+      await this.newTransport().sendMail(mailOptions);
+    } catch (err) {
+      console.log('Error sending email: ', err);
+    }
   }
 
   async sendWelcome() {
