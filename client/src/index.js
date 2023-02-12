b02ea390e9511d8e07e86404d17703115d29a647
Giuliano Gabella <ggabella91@gmail.com>
2023-02-12 10:49:49 -0600

    Client: Update react and react-dom to v18

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -9,7 +9,10 @@ import { store, persistor } from './redux/store';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
